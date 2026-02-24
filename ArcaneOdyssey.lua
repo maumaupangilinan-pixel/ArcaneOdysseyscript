713f16d1ddebbf97f8b0c3f11f0e10d69afc8118
MementoNoMori <maumaupangilinan@gmail.com>
2026-02-25 04:36:35 +0800

    Create ArcaneOdyssey.lua

---
+++ ArcaneOdyssey.lua
@@ -0,0 +1,5 @@
+-- My Arcane Odyssey Script
+print("Hello from my GitHub repo!")
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/violin-suzutsuki/LinoriaLib/main/Library.lua"))()
+local Window = Library:CreateWindow({Title = "My Script", Center = true})
+print("Script loaded successfully!")
